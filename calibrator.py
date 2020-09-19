60379e656a9ac60232be5599121613162b0c1e5e
dkasabovn <dkasabovn@gmail.com>
2020-09-18 23:22:54 -0500

    servo tests 3

--- calibrator.py
+++ calibrator.py
@@ -4,6 +4,6 @@ import busio
 from adafruit_servokit import ServoKit
 import adafruit_motor.servo
 
-i2c_bus = busio.I2C(board.SCL_1, board.SDA_1);
+i2c_bus = busio.I2C(board.SCL, board.SDA);
 kit = ServoKit(channels=16, i2c=i2c_bus)
 kit.servo[0].angle = 0
