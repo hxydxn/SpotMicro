c3ba6d58087b2736fac2202057b53a0a33646113
dkasabovn <dkasabovn@gmail.com>
2020-09-27 19:14:31 -0500

    host nuxt on nano

--- server.py
+++ server.py
@@ -1,11 +1,15 @@
 from sanic import Sanic
-from sanic.response import json
+from sanic.response import json, html
+import os
 
 app = Sanic("jetson_host")
+app.static('/', './dist')
+app.static('/_nuxt', './dist/_nuxt')
 
 @app.route("/")
 async def test(request):
-    return json({"hello": "world"})
+    template = open(os.getcwd() + "/dist/index.html")
+    return html(template.read())
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
