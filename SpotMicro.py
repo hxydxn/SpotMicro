f409df1714d9a4a738d8c094f029c60ded574d19
dkasabovn <dkasabovn@gmail.com>
2020-09-27 20:26:18 -0500

    added basic website

--- SpotMicro.py
+++ SpotMicro.py
@@ -1,25 +1,26 @@
-# import board
-# import digitalio
-# import busio
-# from adafruit_servokit import ServoKit
-# import adafruit_motor.servo
+import board
+import digitalio
+import busio
+from adafruit_servokit import ServoKit
+import adafruit_motor.servo
 
-# i2c_bus = busio.I2C(board.SCL, board.SDA);
-# i2c_bus2 = busio.I2C(board.SCL_1, board.SDA_1);
-# kit = ServoKit(channels=16, i2c=i2c_bus)
-# kit2 = ServoKit(channels=16, i2c=i2c_bus2)
+i2c_bus = busio.I2C(board.SCL, board.SDA);
+i2c_bus2 = busio.I2C(board.SCL_1, board.SDA_1);
+kit = ServoKit(channels=16, i2c=i2c_bus)
+kit2 = ServoKit(channels=16, i2c=i2c_bus2)
 
 class SpotMicroDriver:
 
     def test(self):
         print("test")
-    # def __init__(self, bus_count):
-    #     self.kit = ServoKit(channels=16, i2c=busio.I2C(board.SCL, board.SDA))
-    #     self.kit2 = ServoKit(channels=16, i2c=busio.I2C(board.SCL_1, board.SDA_1))
-    #     super().__init__()
 
-    # def set_servo_angle(self, board_id, port, angle):
-    #     if (board_id == 1):
-    #         self.kit.servo[port].angle = angle
-    #     else:
-    #         self.kit2.servo[port].angle = angle
+    def __init__(self, bus_count):
+        self.kit = ServoKit(channels=16, i2c=busio.I2C(board.SCL, board.SDA))
+        self.kit2 = ServoKit(channels=16, i2c=busio.I2C(board.SCL_1, board.SDA_1))
+        super().__init__()
+
+    def set_servo_angle(self, board_id, port, angle):
+        if (board_id == 1):
+            self.kit.servo[port].angle = angle
+        else:
+            self.kit2.servo[port].angle = angle
--- server.py
+++ server.py
@@ -1,8 +1,13 @@
 from sanic import Sanic
 from sanic.response import json, html
+from sanic_cors import CORS, cross_origin
+from SpotMicro import SpotMicroDriver
 import os
 
+driver = SpotMicroDriver(2)
+
 app = Sanic("jetson_host")
+CORS(app)
 app.static('/', './dist')
 app.static('/_nuxt', './dist/_nuxt')
 
@@ -10,6 +15,14 @@ app.static('/_nuxt', './dist/_nuxt')
 async def test(request):
     template = open(os.getcwd() + "/dist/index.html")
     return html(template.read())
+
+@app.route("/power", methods=["POST", "OPTIONS"])
+async def test_power(request):
+    board = request.json["board"]
+    channel = request.json["channel"]
+    angle = request.json["angle"]
+    driver.set_servo_angle(board, channel, angle)
+    return json({"data": f'{board}-{channel}-{angle}'})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
