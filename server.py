f22890508ecc99e0c80215772e6dbd944d229ea4
dkasabovn <dkasabovn@gmail.com>
2020-10-11 00:53:26 -0500

    functional ui

--- server.py
+++ server.py
@@ -22,7 +22,27 @@ async def test_power(request):
     channel = request.json["channel"]
     angle = request.json["angle"]
     driver.set_servo_angle(board, channel, angle)
-    return json({"data": f'{board}-{channel}-{angle}'})
+    return json({"angle": angle, "board": board, "channel": channel})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
+
+@app.route("/rest", methods=["POST", "OPTIONS"])
+async def set_configuration(request):
+    servos_1 = request.json["PCA9865_1"]
+    servos_2 = request.json["PCA9865_2"]
+    servo_list_1 = [_ for _ in servos_1]
+    servo_list_2 = [_ for _ in servos_2]
+    large_list = servo_list_1 + servo_list_2
+    for servo in large_list:
+        driver.set_servo_angle(servo["board"], servo["port"], servo["angle"])
+    return json({}, status=200)
+
+@app.route("/profile", methods=["POST", OPTIONS])
+async def set_profile(request):
+    servos = request.json["servos"]
+    for servo in servos:
+        angle = servo["rest_angle"]
+        board_id = servo["board"]
+        port = servo["channel"]
+        driver.set_servo_angle(board_id, port, angle)
